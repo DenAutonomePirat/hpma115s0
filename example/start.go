d29c2795a345f4c485d63d18edb1df089a8f1b82
denautonomepirat <appelsinkanon@gmail.com>
2017-11-24 05:45:40 -0800

    ya di dah

---
+++ example/start.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/denautonomepirat/hpma115s0"
+	"github.com/jacobsa/go-serial/serial"
+	"log"
+	"os"
+)
+
+func main() {
+	fmt.Println("morning")
+
+	options := serial.OpenOptions{
+		PortName:        "/dev/ttyACM0",
+		BaudRate:        9600,
+		DataBits:        8,
+		StopBits:        1,
+		MinimumReadSize: 4,
+	}
+
+	// Open the port.
+	port, err := serial.Open(options)
+	if err != nil {
+		log.Fatalf("serial.Open: %v", err)
+	}
+
+	// Make sure to close it later.
+	defer port.Close()
+
+	hpma := hpma115s0.NewHpma11520(port)
+	m := hpma.ReadParticleMeasurement()
+	b, _ := json.Marshal(m)
+	os.Stdout.Write(b)
+	fmt.Println(m.Pm25)
+}
---
+++ hpma115s0.go
@@ -0,0 +1,52 @@
+package hpma115s0
+
+import (
+	"encoding/json"
+	"io"
+	"time"
+)
+
+const (
+	READ_PARTICLE_MEASURMENT    byte = 0x04
+	START_PARTICLE_MEASURMENT   byte = 0x01
+	STOP_PARTICLE_MEASURMENT    byte = 0x02
+	SET_ADJUSTMENT_COEFFICIENT  byte = 0x08
+	READ_ADJUSTMENT_COEFFICIENT byte = 0x08
+	STOP_AUTO_SEND              byte = 0x20
+	ENABLE_AUTO_SEND            byte = 0x40
+)
+
+type Measurement struct {
+	TimeStamp int64 `json:"timestamp"`
+	Pm25      int   `json:"pm25"`
+	Pm10      int   `json:"pm10"`
+}
+
+func (m *Measurement) Marshal() *[]byte {
+	encoded, _ := json.Marshal(m)
+	return &encoded
+}
+
+type Hpma115s0 struct {
+	Port io.ReadWriteCloser
+}
+
+func NewHpma11520(p io.ReadWriteCloser) *Hpma115s0 {
+	h := Hpma115s0{
+		Port: p,
+	}
+	return &h
+}
+
+func (h *Hpma115s0) ReadParticleMeasurement() *Measurement {
+	m := Measurement{
+		TimeStamp: int64(time.Now().UnixNano() / 1000 / 1000),
+		Pm25:      4,
+		Pm10:      1,
+	}
+	return &m
+}
+
+func (h *Hpma115s0) Close() {
+	h.Port.Close()
+}
